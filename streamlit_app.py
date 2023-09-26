d4f4adc657c9fff8dd561a0364a9f8cae26e1a8e
Carrie Beesley <carrie@carriebeesley.com>
2023-09-26 10:16:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 import pandas
 
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text('🫐 Omega 3 Blueberry Oatmeal')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
