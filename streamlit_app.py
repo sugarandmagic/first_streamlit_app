fe6ed2c13faab1bd853aad45d6f2ea12ecbd739b
Carrie Beesley <carrie@carriebeesley.com>
2023-09-26 08:09:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.dataframe(my_fruit_list)
 
@@ -17,4 +18,3 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Strawberries', 'Avocado'])
 
 # Display the table on the page.
-my_fruit_list = my_fruit_list.set_index('Fruit')
